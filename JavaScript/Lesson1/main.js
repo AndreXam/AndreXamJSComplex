038ea33d4460e8f053e4fc90442fdfccb9052366
AndreXam <andrexam88@gmail.com>
2022-11-02 23:01:29 +0200

    Homework1 Js (fixed)

--- JavaScript/Lesson1/main.js
+++ JavaScript/Lesson1/main.js
@@ -9,17 +9,41 @@ let num3 = -999;
 let num4 = 123;
 let num5 = 2.7;
 let num6 =16;
-let PI = 3.14;
+let pi = 3.14;
 
-let Front = true;
-let Back = false
+let front = true;
+let back = false
 
-let FirstName = "Andriy";
-let MiddleName = "Mykhailovich";
-let LastName = "Khomyn";
+let firstName = "Andriy";
+let middleName = "Mykhailovich";
+let lastName = "Khomyn";
 
-let pib = FirstName + ' ' + MiddleName + ' ' + LastName
+let pib = firstName + ' ' + middleName + ' ' + lastName
 
 let a = 100;
 let b = '100';
 let c = true
+
+
+console.log(hi);
+console.log(owu);
+console.log(its);
+console.log(me);
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(pi);
+
+console.log(front);
+console.log(back);
+
+
+console.log(pib);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
