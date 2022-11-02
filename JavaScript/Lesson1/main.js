802e649de784dcbaff5706a286fb7d9c01310b08
AndreXam <andrexam88@gmail.com>
2022-11-02 22:37:13 +0200

    Homework1 Js

---
+++ JavaScript/Lesson1/main.js
@@ -0,0 +1,25 @@
+let hi = "Hello";
+let owu = "OWU";
+let its = "com";
+let me = "ua";
+
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 2.7;
+let num6 =16;
+let PI = 3.14;
+
+let Front = true;
+let Back = false
+
+let FirstName = "Andriy";
+let MiddleName = "Mykhailovich";
+let LastName = "Khomyn";
+
+let pib = FirstName + ' ' + MiddleName + ' ' + LastName
+
+let a = 100;
+let b = '100';
+let c = true
