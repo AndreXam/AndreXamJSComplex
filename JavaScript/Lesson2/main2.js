a6f36b14fba11a4da18a5db027c45bcfbe71e9ca
AndreXam <andrexam88@gmail.com>
2022-11-04 19:52:30 +0200

    Homework2 Js arrays and objects

---
+++ JavaScript/Lesson2/main2.js
@@ -0,0 +1,69 @@
+let arr = [ 21,true,-65,'Gummy',false,'fall',324,'final move',true,'Fatality']
+console.log(arr)
+
+let book1 = {
+    title : 'Volkswagen',
+    pageCount : 710,
+    genre: 'techical book',
+}
+console.log(book1);
+
+let book2 = {
+    title : 'Witcher',
+    pageCount : 250 ,
+    genre: 'fantasy'
+}
+console.log(book2);
+let book3 ={
+    title: 'Ctulchu',
+    pageCount : 658,
+    genre: 'horror, mistery'
+}
+console.log(book3);
+
+let tech = {
+    title: 'Volkswagen',
+    pageCount: 710,
+    genre: 'techical book',
+    authors: [{name:'Das Auto', age: 80 }],
+}
+console.log(tech);
+
+let fant = {
+    title : 'Witcher',
+    pageCount : 250 ,
+    genre: 'fantasy',
+    authors: [{ name:'Andjei Sapkowskiy', age: 80 }],
+    }
+    console.log(fant);
+
+let mist = {
+    title: 'Ctulchu',
+    pageCount : 658,
+    genre: 'horror, mistery',
+    authors: [{name:'Lovecraft', age:100}],
+}
+console.log(mist);
+
+let huge = [
+    {name:'Pablo', username:'Juan',password:'4532s'},
+    {name:'Rick', username:'Sanchez',password:'42455tr'},
+    {name:'Mika', username:'Rika',password:525352},
+    {name:'Peter', username:'Parker',password:'Trtggs'},
+    {name:'Miles', username:'Morales',password:5326662},
+    {name:'Eddie', username:'Brock',password:'sistemiko'},
+    {name:'Otto', username:'Octavius',password:'octo'},
+    {name:'Green', username:'Go',password:'greengo'},
+    {name:'Jamie', username:'Johnson',password:'5tsty'},
+    {name:'Pika', username:'Chu',password:'uoiu'},
+]
+console.log(huge[0].password);
+console.log(huge[1].password);
+console.log(huge[2].password);
+console.log(huge[3].password);
+console.log(huge[4].password);
+console.log(huge[5].password);
+console.log(huge[6].password);
+console.log(huge[7].password);
+console.log(huge[8].password);
+console.log(huge[9].password);
